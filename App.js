5d53f7214edd086aad766d5fb2f38a01856a8cb2
Leonardo Salles <leonardosalles@Leonardos-MBP.southsystem.local>
2020-03-03 18:15:57 -0300

    add env support.

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import * as React from 'react'
 import { AppThemeProvider } from '@app/themes'
 import Layout from '@app/components/Layout'
+import { API_URL } from 'react-native-dotenv'
+
+console.log(API_URL)
 
 export default function App() {
   return (
